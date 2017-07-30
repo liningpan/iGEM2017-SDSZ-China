fc5c96dba006454914c71611f76de85e270e02c1
David Jiang <jianglin332@outlook.com>
2017-07-30 15:10:38 +0800

    refactored

---
+++ ccd/ftdevice.cpp
@@ -0,0 +1,6 @@
+#include "ftdevice.h"
+
+ftdevice::ftdevice()
+{
+
+}
---
+++ ccd/ftdevice.h
@@ -0,0 +1,11 @@
+#ifndef FTDEVICE_H
+#define FTDEVICE_H
+
+
+class ftdevice
+{
+public:
+    ftdevice();
+};
+
+#endif // FTDEVICE_H
